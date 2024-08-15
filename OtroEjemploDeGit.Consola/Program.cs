474473a7dacda222ca7750d0ccc2993f0c9f207e
CarlosCatedras <catedrasinsituto43@gmail.com>
2024-08-15 11:56:55 -0300

    Agregar archivos de proyecto.

---
+++ OtroEjemploDeGit.Consola/Program.cs
@@ -0,0 +1,19 @@
+﻿
+namespace OtroEjemploDeGit.Consola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Resolución del Problema");
+            Console.Write("Ingrese un número:");
+            var numeroIngresado = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese multiplicador:");
+            var multiplicador = int.Parse(Console.ReadLine());
+            var multiplo = Multiplicar(numeroIngresado, multiplicador);
+            Console.WriteLine($"{numeroIngresado} multiplicado por {multiplicador} es {multiplo}");
+        }
+
+        private static int Multiplicar(int numero, int numeroPor) => numero * numeroPor;
+    }
+}
